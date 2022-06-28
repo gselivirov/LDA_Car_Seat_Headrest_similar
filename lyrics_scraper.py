322fa15dc2ff5eb1b45be50cd949bd37d051ac75
gselivirov <gselivirov@gmail.com>
2022-06-28 17:35:26 +0400

    lyrics scraper

--- lyrics_scraper.py
+++ lyrics_scraper.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import re
+import os
+import glob
+
 
 link_list=[]
 lyrics_list=[]
@@ -23,9 +26,28 @@ for link in links:
 
 
 d={"lyrics":lyrics_list}
-
 lyrics_data=pd.DataFrame(d)
-
 lyrics_data.to_csv("lyrics.csv", index=False)
 
 
+path="lyrics"
+csv_files = glob.glob(os.path.join(path, "*.csv"))
+to_conc = [pd.read_csv(file) for file in csv_files]
+df = pd.concat(to_conc)
+df.reset_index(drop=True, inplace=True)
+df.insert(1,"songs","")
+
+
+i = 0
+for row in df["lyrics"]:
+    if pd.notnull(row):
+        lyr = row.replace("\n", " ")
+        lyr = re.sub(r"(\d{0,2})Embed", "", lyr)
+        nlyr = lyr.split("Lyrics", maxsplit=1)
+        df["songs"][i]=nlyr[0]
+        df["lyrics"][i]=nlyr[1]
+    else: lyr = None
+    i=i+1
+
+
+df.to_csv("combined_lyrics.csv", index=False)
